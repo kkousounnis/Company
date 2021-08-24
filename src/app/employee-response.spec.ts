a58836ea67b9ba7d0135f37708d9fcd78d1afcbe
konstantinos.kousounnis <kousounnisk@gmail.com>
2021-08-24 03:36:02 -0700

    Remove

--- src/app/employee-response.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeeResponse } from './employee-response';
-
-describe('EmployeeResponse', () => {
-  it('should create an instance', () => {
-    expect(new EmployeeResponse()).toBeTruthy();
-  });
-});
