d0c32e2ad7f0e2b2e924f5f89a65610a9b10e86b
kkousounnis <kousounnisk@gmail.com>
2021-08-27 11:03:52 +0300

    Added database

---
+++ company.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `kkcompany`;
+USE `kkcompany`;
+
+CREATE TABLE `tb_emp`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(255) DEFAULT NULL,
+    `last_name` VARCHAR(255) DEFAULT NULL,
+    `email_id` VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY(`id`)
+);
+
+INSERT INTO `tb_emp` (`id`, `first_name`, `last_name`, `email_id`) VALUES('1','Mukund ','Madhav','mukund@gmail.com');
+INSERT INTO `tb_emp` (`id`, `first_name`, `last_name`, `email_id`) VALUES('2','Ali ','Alentai','ali@gmail.com');
